4300e9904d0e3cf9a34e8df25be22302054dd946
Lisa Grumbach <grumbach@uni-trier.de>
2022-04-06 10:35:21 +0200

    implement json graph persistency

--- src/data/operator/saveFiles/index.js
+++ src/data/operator/saveFiles/index.js
@@ -3,6 +3,10 @@ const saveFiles = [
 		name: "Save File 1",
 		file: () => fetch("./saveFiles/saveFile1.json").then((response) => response.json()),
 	},
+	{
+	name: "Verkehrsunfall",
+	file: () => fetch("./saveFiles/unfall.json").then((response) => response.json()),
+	}
 ];
 
 export { saveFiles };
