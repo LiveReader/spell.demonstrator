2b24db66c9c8ead9a187a96fcda3739602c6ab0c
Jason Rietzke <jason.rietzke@livereader.com>
2022-04-07 12:07:56 +0200

    implemented: taxonomy prefix generation and parsing

--- src/data/operator/taxonomy/index.js
+++ src/data/operator/taxonomy/index.js
@@ -40,6 +40,50 @@ function findID(obj, id) {
 	return null;
 }
 
+function generatePrefixedTaxonomy() {
+	const taxonomyCopy = JSON.parse(JSON.stringify(taxonomyTemplate));
+	return generatePrefixes(taxonomyCopy);
+}
+function generatePrefixes(tax) {
+	if (tax == null || Array.isArray(tax) || typeof tax !== "object") return tax;
+	const keys = Object.keys(tax);
+	for (let i = 0; i < keys.length; i++) {
+		const key = keys[i];
+		tax[key] = generatePrefixes(tax[key]);
+	}
+	if (tax.prefix) {
+		for (let i = 0; i < keys.length; i++) {
+			const key = keys[i];
+			if (key == "prefix" || key == "label" || key == "id") continue;
+			tax[tax.prefix + key] = tax[key];
+			delete tax[key];
+		}
+	}
+	return tax;
+}
+
+function parsePrefixedTaxonomy(taxonomy) {
+	const taxonomyCopy = JSON.parse(JSON.stringify(taxonomy));
+	return removePrefixes(taxonomyCopy);
+}
+function removePrefixes(tax) {
+	if (tax == null || Array.isArray(tax) || typeof tax !== "object") return tax;
+	const keys = Object.keys(tax);
+	for (let i = 0; i < keys.length; i++) {
+		const key = keys[i];
+		tax[key] = removePrefixes(tax[key]);
+	}
+	if (tax.prefix) {
+		for (let i = 0; i < keys.length; i++) {
+			const key = keys[i];
+			if (key == "prefix" || key == "label" || key == "id") continue;
+			tax[key.replace(tax.prefix, "")] = tax[key];
+			delete tax[key];
+		}
+	}
+	return tax;
+}
+
 generateID(taxonomyTemplate);
 
-export { taxonomyTemplate, generateID, findID };
+export { taxonomyTemplate, generateID, findID, generatePrefixedTaxonomy, parsePrefixedTaxonomy };
