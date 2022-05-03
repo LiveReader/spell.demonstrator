0859351b3edc0a35f6e63891061acf68f03c67e5
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-03 16:45:20 +0200

    implemented: api converter
    converts the api response tree to local graph

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import * as SpellApi from "open_api_definition";
 const spellApi = new SpellApi.DefaultApi();
 
-import { prefixedTaxonomyTemplate, parsePrefixedTaxonomy } from "../data/operator/taxonomy/index";
+import { prefixedTaxonomyTemplate, findTaxEntry } from "../data/operator/taxonomy/index";
 
 const NodeType = {
 	Operation: "operation",
@@ -19,6 +19,29 @@ class API {
 		this.bpInstancePrefix = "http://www.semanticweb.org/bpinstance#";
 		this.bpDomainPrefiox = "http://www.semanticweb.org/bpdomain#";
 
+		spellApi.loadScenarios(
+			JSON.stringify({
+				id: [0],
+			}),
+			(err, data, res) => {
+				spellApi.loadScenarios(
+					JSON.stringify({
+						id: [1],
+					}),
+					(err, data, res) => {
+						spellApi.loadScenarios(
+							JSON.stringify({
+								id: [2],
+							}),
+							(err, data, res) => {
+								console.log(res);
+							}
+						);
+					}
+				);
+			}
+		);
+
 		this.operations = [];
 	}
 
@@ -27,42 +50,89 @@ class API {
 			for (let i in data.nodes) {
 				this.operations.push(data.nodes[i]);
 			}
-			for (let i in this.operations) {
-				const operation = this.operations[i];
-				this.getOperation(operation.id, (data) => {
-					operation.node = data;
-					if (i == this.operations.length - 1) {
-						return this.operations;
-					}
-				});
-			}
 		});
 	}
 
-	getOperation(id, callback) {
+	getOperation(id, callback = () => {}) {
 		spellApi.getDataObject(id, (err, data, res) => {
-			callback(data);
+			const graph = this.convertToGraph(data);
+			const operation = this.operations.find((o) => o.id === id);
+			operation.graph = graph;
+			callback(operation);
 		});
 	}
 
 	convertToGraph(data) {
 		const graph = {
 			nodes: [],
-			edges: [],
+			links: [],
 		};
-		// id is data.id after "#" in url
-		const id = data.id.split("#")[1];
-		const type = data.type.id.split("#")[1];
-		// check if type is included in NodeType
-		if (!Object.values(NodeType).includes(type)) {
-			console.log("Taxonomy Entry");
-		} else {
-			console.log("Node Type");
+		convert(data, graph);
+		return graph;
+
+		function convert(d, g, source = null) {
+			// id is data.id after "#" in url
+			const type = (d?.type?.id ?? "").split("#")[1];
+			// check if type is included in NodeType
+			if (!Object.values(NodeType).includes(type)) {
+				convertTax(d, g, source);
+			} else {
+				convertNode(d, g, source);
+			}
+		}
+
+		function convertNode(d, g, source = null) {
+			const id = d.id.split("#")[1];
+			const type = d.type.id.split("#")[1];
 			const shape = {
 				type: type,
 				scale: 1,
 			};
 			const taxonomy = JSON.parse(JSON.stringify(prefixedTaxonomyTemplate))[type];
+			const node = {
+				id: id,
+				shape: shape,
+				payload: {},
+				taxonomy: taxonomy,
+			};
+			graph.nodes.push(node);
+			if (source) {
+				const link = {
+					source: source.id,
+					target: id,
+					type: "solid",
+					directed: false,
+					label: "",
+					strength: "weak",
+				};
+				graph.links.push(link);
+			}
+			for (let i in d["attributes"]) {
+				const attr = d["attributes"][i];
+				convertAttribute(attr, node);
+			}
+			for (let i in d["dataObjects"]) {
+				const child = d["dataObjects"][i];
+				convert(child, g, node);
+			}
+		}
+
+		function convertTax(d, g, source = null) {
+			for (let i in d["attributes"]) {
+				const attr = d["attributes"][i];
+				convertAttribute(attr, source);
+			}
+			for (let i in d["dataObjects"]) {
+				const child = d["dataObjects"][i];
+				convert(child, g, source);
+			}
+		}
+
+		function convertAttribute(attr, source) {
+			const name = (attr?.type?.id ?? "").split("#")[1];
+			const entry = findTaxEntry(source.taxonomy, name);
+			if (!entry) return;
+			entry.value = attr.value;
 		}
 	}
 }
