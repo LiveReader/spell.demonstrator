c52bfa8ebb4bb47fa25e544439f0ae946523f86a
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-04 11:41:25 +0200

    fixed: json transfer limit

--- server/src/main.js
+++ server/src/main.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use("/operation", operationRouter);
 app.use("/scenario", scenarioRouter);
 
