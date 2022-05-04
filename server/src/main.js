606ea9997ba111f7467898d0f3b47db50bea940d
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-04 02:02:59 +0200

    implemented: operation sharing server

---
+++ server/src/main.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+import operationRouter from "./routers/operation.js";
+import scenarioRouter from "./routers/scenario.js";
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use("/operation", operationRouter);
+app.use("/scenario", scenarioRouter);
+
+app.listen(port);
