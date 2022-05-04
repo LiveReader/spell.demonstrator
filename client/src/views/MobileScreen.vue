f90d3b540bacf07571b7820a8e29477425c2417f
Jason Rietzke <jason.rietzke@livereader.com>
2022-05-04 15:24:19 +0200

    implemented: MobileScreen API handling

--- client/src/views/MobileScreen.vue
+++ client/src/views/MobileScreen.vue
@@ -1,42 +1,16 @@
 <template>
 	<div id="mobile-screen">
-		<Navigation
-			:color="'#ffb74d'"
-			:icon="'./spell.demonstrator.einsatzkräfte.svg'"
-			:title="'Einsatzkräfte'"
-		>
-		<v-list density="compact" nav color="#00000000">
-				<v-list-group>
-					<template #activator="{ props }">
-						<v-list-item
-							v-bind="props"
-							prepend-icon="mdi-code-json"
-							title="Save Files"
-							value="safefileCollapsed"
-						></v-list-item>
-					</template>
-					<v-list-item
-						v-for="(item, index) in saveFiles"
-						:key="item"
-						prepend-icon="mdi-file"
-						:title="item.name ?? index"
-						rounded="xl"
-						@click="selectedCase(item)"
-					></v-list-item>
-				</v-list-group>
-			</v-list>
+		<Navigation :color="'#ffb74d'" :icon="'./spell.demonstrator.einsatzkräfte.svg'" :title="'Einsatzkräfte'">
 			<v-list density="compact" nav color="#00000000">
 				<v-list-group>
 					<template #activator="{ props }">
-						<v-list-item
-							v-bind="props"
-							title="Select emergency-ressource"
-						></v-list-item>
+						<v-list-item v-bind="props" title="Select emergency-ressource"></v-list-item>
 					</template>
-					<v-list-item v-for="(item, index) in ressource" 
-					:key="index"
-					:title="item ?? index"
-					@click="Nr = item;crtgraph(b)"
+					<v-list-item
+						v-for="(item, index) in ressource"
+						:key="item.node.id"
+						:title="item.node.payload.label ?? index"
+						@click="selectRessource(item)"
 					></v-list-item>
 				</v-list-group>
 			</v-list>
@@ -44,183 +18,290 @@
 		<Graphly
 			:graph="graph"
 			:selected="selection"
+			:gravity="0"
 			@double-click="onDoubleClick"
 			@click="onClick"
 			@background="onBackground"
 		/>
-		<v-navigation-drawer permanent position="right" style="height: 65px">
-			<v-card>
-				<v-btn
-					id="btn"
-					:color="'#ffb74d'"
-					v-for="item in btns"
-					:key="item"
-					:icon="item.icon"
-					@click="btns_click(item.name)">
-				</v-btn>
-			</v-card>
-		</v-navigation-drawer>
+		<SideBar width="190" color-sheme="#ffb74d" :control-items="controlItems" :show-questions="false" />
 		<NodeModal :modal="modal" @close="onCloseModal" />
 	</div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { saveFiles } from "../data/operator/saveFiles/index";
-import Navigation from "./Navigation.vue";
-import Graphly from "../components/Graphly.vue";
-import NodeModal from "../components/NodeModal.vue";
+import { ref, onMounted } from "vue";
 import { taxonomy2payload } from "../data/operator/converter/index";
 import { taxonomyTemplate, parsePrefixedTaxonomy } from "../data/operator/taxonomy/index";
+import { loadOperation, putOperation } from "../api/index";
 
-let graph = ref({ nodes: [], links: [], hasUpdate: false });
-let dat = ref({ nodes: [], links: [], hasUpdate: false });
-let Nr = ref("ABC012")
-let len = ref(0)
-let b = ref()
-let ressource = ref ([])
-let modal = ref({ show: false, node: null, title: "" });
-let btns = ref([{name: "Person hinzufügen", icon: "mdi-account"},{name: "Objekt hinzufügen", icon: "mdi-home"},{name: "löschen", icon: "mdi-delete"}])
-let new_id = ref("new_id_0")
-let selection = ref([])
+import Navigation from "./Navigation.vue";
+import Graphly from "../components/Graphly.vue";
+import SideBar from "../components/SideBar.vue";
+import NodeModal from "../components/NodeModal.vue";
 
-function availableressources (data){
-	ressource.value = []
-	dat.value = data
-	for (let h= 0; h < dat.value.nodes.length; h++){
-		if (dat.value.nodes[h].shape.type == "emergency-ressource"){
-			ressource.value.push (dat.value.nodes[h].payload.label)
-		}
-	}
+const operations = ref([]);
+const operationID = ref("");
+function loadOperations(callback = () => {}) {
+	fetch("http://localhost:8080/operation/all", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			operations.value = data;
+			callback();
+		});
 }
-async function creategraph (data) {
-	graph.value = ({ nodes: [], links: [], hasUpdate: true })
-	dat.value = data
-	for (let h = 0; h < dat.value.nodes.length; h++){
-		if (dat.value.nodes[h].payload.label == Nr.value){
-			const ressource = {
-				id: dat.value.nodes[h].id,
-				anchor: {"type": "hard","x": -100,"y": 0},
-				payload: dat.value.nodes[h].payload,
-				shape: dat.value.nodes[h].shape,
-				taxonomy: dat.value.nodes[h].taxonomy,
-			}
-			graph.value.nodes.push(ressource)
-			graph.value.nodes[0].shape.scale = 1
-		}
-	}
-	for (let i = 0; i < dat.value.links.length; i++){
-		if (dat.value.links[i].source == graph.value.nodes[0].id){
-			for (let j = 0; j < dat.value.nodes.length; j++){
-				if (dat.value.links[i].target == dat.value.nodes[j].id){
-					if (dat.value.nodes[j].shape.type == "emergency-action"){
-						const ressource = {
-							anchor: {"type": "hard","x": 100,"y": 200},
-							id: dat.value.nodes[j].id,
-							payload: dat.value.nodes[j].payload,
-							shape: dat.value.nodes[j].shape,
-							taxonomy: dat.value.nodes[j].taxonomy,
-						}
-						graph.value.nodes.push(ressource)
-					}
-					if (dat.value.nodes[j].shape.type == "operation"){
-						const ressource = {
-							anchor: {"type": "hard","x": -300,"y": -200},
-							id: dat.value.nodes[j].id,
-							payload: dat.value.nodes[j].payload,
-							shape: dat.value.nodes[j].shape,
-							taxonomy: dat.value.nodes[j].taxonomy,
-						}
-						graph.value.nodes.push(ressource)
-					}
-					graph.value.links.push(dat.value.links[i])
-				}
-			}
-		}
-	}
-	len = graph.value.nodes.length
-	for (let k = 1; k < len; k++){
-		for (let i = 0; i < dat.value.links.length; i++){
-			if (dat.value.links[i].target == graph.value.nodes[k].id && dat.value.links[i].source != graph.value.nodes[0].id){
-				for (let j = 0; j < dat.value.nodes.length; j++){
-					if (dat.value.links[i].source == dat.value.nodes[j].id){
-						if(dat.value.nodes[j].shape.type == "affected-person"){
-							const ressource = {
-								anchor: {"type": "soft","x": 100,"y": 200},
-								id: dat.value.nodes[j].id,
-								payload: dat.value.nodes[j].payload,
-								shape: dat.value.nodes[j].shape,
-								taxonomy: dat.value.nodes[j].taxonomy,
-							}
-							graph.value.nodes.push(ressource)
-						}
-						if(dat.value.nodes[j].shape.type == "affected-object"){
-							const ressource = {
-								anchor: {"type": "soft","x": 100,"y": 200},
-								id: dat.value.nodes[j].id,
-								payload: dat.value.nodes[j].payload,
-								shape: dat.value.nodes[j].shape,
-								taxonomy: dat.value.nodes[j].taxonomy,
-							}
-							graph.value.nodes.push(ressource)
-						}
-						if(dat.value.nodes[j].shape.type == "emergency-reporter"){
-							continue
-						}
-						//graph.value.nodes.push(dat.value.nodes[j])
-						graph.value.links.push(dat.value.links[i])
-					}
-				}
-			}
-		}
-	}
-	for (let j = 0; j < dat.value.nodes.length; j++){
-		//if(dat.value.nodes[j].payload.label == "Operation" && (typeof graph.value.nodes[1] == "undefined" || graph.value.nodes[1].id != dat.value.nodes[j].id)){
-		if(dat.value.nodes[j].payload.label == "Operation" && graph.value.nodes[1].id != dat.value.nodes[j].id){
-			graph.value.nodes.push(dat.value.nodes[j])
-			graph.value.nodes[graph.value.nodes.length-1].anchor = {"type": "hard","x": -300,"y": -200}
-			const link = {
-				source: dat.value.nodes[j].id,
-				target: graph.value.nodes[0].id,
+function openOperation(id) {
+	operationID.value = id;
+}
+function updateOperation() {
+	putOperation(rawGraph);
+}
+
+let ressourceID = ref("");
+let ressource = ref([]);
+let rawGraph = ref({ nodes: [], links: [], hasUpdate: false });
+let graph = ref({ nodes: [], links: [], hasUpdate: false });
+let selection = ref([]);
+let modal = ref({ show: false, node: null, title: "" });
+let controlItems = ref([
+	{
+		icon: "mdi-account",
+		enabled: true,
+		checkEnabled: () => true,
+		onClick: () => {
+			const tax = taxonomyTemplate["affected-person"];
+			const node = {
+				id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+				shape: {
+					type: "affected-person",
+					scale: 1,
+				},
+				payload: {},
+				taxonomy: tax,
+			};
+			taxonomy2payload[node.shape.type](node, rawGraph.value);
+			rawGraph.value.nodes.push(node);
+			rawGraph.value.links.push({
+				source: graph.value.nodes.find((n) => n.shape.type == "operation").id,
+				target: node.id,
 				type: "solid",
 				directed: false,
-				strength: "weak",
+				strength: "loose",
+			});
+			console.log(ressourceID);
+			rawGraph.value.links.push({
+				source: node.id,
+				target: graph.value.nodes.find((n) => n.shape.type == "emergency-action").id,
+				type: "solid",
+				directed: false,
+				strength: "loose",
+			});
+			updateOperation();
+		},
+	},
+	{
+		icon: "mdi-home",
+		enabled: true,
+		checkEnabled: () => true,
+		onClick: () => {
+			const tax = taxonomyTemplate["affected-object"];
+			const node = {
+				id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+				shape: {
+					type: "affected-object",
+					scale: 1,
+				},
+				payload: {},
+				taxonomy: tax,
+			};
+			taxonomy2payload[node.shape.type](node, rawGraph.value);
+			rawGraph.value.nodes.push(node);
+			rawGraph.value.links.push({
+				source: graph.value.nodes.find((n) => n.shape.type == "operation").id,
+				target: node.id,
+				type: "solid",
+				directed: false,
+				strength: "loose",
+			});
+			rawGraph.value.links.push({
+				source: node.id,
+				target: graph.value.nodes.find((n) => n.shape.type == "emergency-action").id,
+				type: "solid",
+				directed: false,
+				strength: "loose",
+			});
+			updateOperation();
+		},
+	},
+	{
+		icon: "mdi-delete",
+		enabled: false,
+		isDelete: true,
+		checkEnabled: () => {
+			const node = graph.value.nodes.find((n) => n.id == selection.value[0]);
+			return (
+				(node.shape.type == "affected-person" || node.shape.type == "affected-object") &&
+				graph.value.nodes.filter((n) => n.shape.type == "affected-person" || n.shape.type == "affected-object")
+					.length > 1 &&
+				selection.value.length > 0
+			);
+		},
+		onClick: () => {
+			const node = graph.value.nodes.find((n) => n.id == selection.value[0]);
+			if (node.shape.type == "affected-person" || node.shape.type == "affected-object") {
+				// check if it is the last node of affected person or object
+				rawGraph.value.links = rawGraph.value.links.filter((l) => l.source != node.id && l.target != node.id);
+				rawGraph.value.nodes = rawGraph.value.nodes.filter((n) => n.id != node.id);
+				updateOperation();
 			}
-			graph.value.links.push(link)
+		},
+	},
+]);
+
+function availableRessources() {
+	ressource.value = [];
+	operations.value.forEach((operation) => {
+		for (let i = 0; i < operation.nodes.length; i++) {
+			const node = operation.nodes[i];
+			if (node.shape.type == "emergency-ressource") {
+				ressource.value.push({
+					operation: operation,
+					node: node,
+				});
+			}
 		}
+	});
+}
+
+function selectRessource(ressource) {
+	ressourceID.value = ressource.node.id;
+	openOperation(ressource.operation.nodes[0].id);
+}
+
+function createGraph() {
+	graph.value = { nodes: [], links: [], hasUpdate: false };
+
+	// create ressource
+	const rawRessourceNode = rawGraph.value.nodes.find((n) => n.id == ressourceID.value);
+	const ressourceNode = {
+		id: rawRessourceNode.id,
+		shape: rawRessourceNode.shape,
+		anchor: {
+			type: "hard",
+			x: 0,
+			y: 0,
+		},
+		payload: rawRessourceNode.payload,
+		taxonomy: rawRessourceNode.taxonomy,
+	};
+	graph.value.nodes.push(ressourceNode);
+
+	// create source nodes (emergency actions)
+	const ressourceSourcesLinks = rawGraph.value.links.filter((l) => l.source == ressourceID.value);
+	const ressourceSources = ressourceSourcesLinks.map((l) => rawGraph.value.nodes.find((n) => n.id == l.target));
+	const ressourceTargetLinks = rawGraph.value.links.filter((l) => l.target == ressourceID.value);
+	const ressourceTargets = ressourceTargetLinks.map((l) => rawGraph.value.nodes.find((n) => n.id == l.source));
+	const ressourceNodes = ressourceSources.concat(ressourceTargets);
+	ressourceNodes.forEach((source, i) => {
+		const sourceNode = {
+			id: source.id,
+			shape: source.shape,
+			spawn: {
+				source: ressourceNode.id,
+				angle: -60,
+				distance: 300 * (i + 1),
+			},
+			payload: source.payload,
+			taxonomy: source.taxonomy,
+		};
+		graph.value.nodes.push(sourceNode);
+		graph.value.links.push({
+			source: source.id,
+			target: ressourceNode.id,
+			type: "solid",
+			directed: false,
+			label: "",
+			strength: "loose",
+		});
+	});
+
+	// create operation node
+	if (graph.value.nodes.find((n) => n.id == operationID.value) == undefined) {
+		const rawOperationNode = rawGraph.value.nodes.find((n) => n.id == operationID.value);
+		const operationNode = {
+			id: rawOperationNode.id,
+			shape: rawOperationNode.shape,
+			spawn: {
+				source: ressourceNode.id,
+				angle: 0,
+				distance: 300,
+			},
+			payload: rawOperationNode.payload,
+			taxonomy: rawOperationNode.taxonomy,
+		};
+		graph.value.nodes.push(operationNode);
+		graph.value.links.push({
+			source: operationNode.id,
+			target: ressourceNode.id,
+			type: "solid",
+			directed: false,
+			label: "",
+			strength: "loose",
+		});
 	}
-	for (let i = 0; i < graph.value.nodes.length; i++) {
-		const node = graph.value.nodes[i];
-		node.taxonomy = parsePrefixedTaxonomy(node.taxonomy);
-		taxonomy2payload[node.shape.type](node, graph.value);
-	}
+
+	// create affected persons & objects
+	ressourceNodes.forEach((source) => {
+		if (!source.shape.type == "affected-person" && !source.shape.type == "affected-object") return;
+		const sourceLinks = rawGraph.value.links.filter((l) => l.target == source.id);
+		const sourceNodes = sourceLinks.map((l) => rawGraph.value.nodes.find((n) => n.id == l.source));
+		for (let i in sourceNodes) {
+			const sn = sourceNodes[i];
+			if (sn.shape.type != "affected-person" && sn.shape.type != "affected-object") continue;
+			console.log(sn);
+			const sourceNode = {
+				id: sn.id,
+				shape: sn.shape,
+				spawn: {
+					source: sn.id,
+					angle: 120,
+					distance: 300,
+				},
+				payload: sn.payload,
+				taxonomy: sn.taxonomy,
+			};
+			graph.value.nodes.push(sourceNode);
+			graph.value.links.push({
+				source: ressourceID.value,
+				target: sn.id,
+				type: "solid",
+				directed: false,
+				label: "",
+				strength: "loose",
+			});
+		}
+	});
+
 	graph.value.hasUpdate = true;
 }
-function openSaveFile(item) {
-	item.file().then((f) => {
-		availableressources(f)
 
-		creategraph(f)
-	});
-}
-function crtgraph(item) {
-	item.file().then((f) => {
-		creategraph(f)
-	});
-}
-function selectedCase(a){
-	Nr.value = "ABC012"
-	b.value = a
-	openSaveFile(a)
-}
 function onCloseModal(d) {
-	// taxonomy2payload[d.shape.type](d, graph.value);
-	// selectedNodes.value = [d.id];
-	// graph.value.hasUpdate = true;
-	// generateOpenQuestions();
-	// filterQuestions();
 	taxonomy2payload[d.shape.type](d, graph.value);
 	graph.value.hasUpdate = true;
+	controlItems.value.forEach((item) => {
+		item.enabled = item.checkEnabled();
+	});
+	updateOperation();
+}
+function onClick(e, d) {
+	selection.value = [d.id];
+	controlItems.value.forEach((item) => {
+		item.enabled = item.checkEnabled();
+	});
 }
 function onDoubleClick(e, d) {
 	if (d.shape.type == "assessment") return;
@@ -237,58 +318,30 @@ function onDoubleClick(e, d) {
 	}
 	modal.value.title = title;
 }
-function btns_click(g){
-	if (g == "Person hinzufügen"){
-		const affected = {
-			anchor: {"type": "soft","x": 100,"y": 200},
-			id: new_id.value,		
-			shape: {type: "affected-person", "scale": 1},
-			payload: {status: "delayed", name: {first: "",last: "",}, sex: "", age: "", accessibility: "", tag: []},
-			taxonomy: JSON.parse(JSON.stringify(taxonomyTemplate["affected-person"] ?? {})),
-		}
-		const link = {
-				source: affected.id,
-				target: graph.value.nodes[1].id,
-				type: "solid",
-				directed: false,
-				strength: "weak",
-			}
-		new_id.value = new_id.value + 1
-		graph.value.nodes.push(affected)
-		graph.value.links.push(link)
-	}
-	if (g == "Objekt hinzufügen"){
-		const affected = {
-			anchor: {"type": "soft","x": 100,"y": 200},
-			id: new_id.value,		
-			shape: {type: "affected-object", "scale": 1},
-			payload: {status: "delayed", label: "", accessibility: "", tag: []},
-			taxonomy: JSON.parse(JSON.stringify(taxonomyTemplate["affected-object"] ?? {})),
-		}
-		const link = {
-				source: affected.id,
-				target: graph.value.nodes[1].id,
-				type: "solid",
-				directed: false,
-				strength: "weak",
-			}
-		new_id.value = new_id.value + "1"
-		graph.value.nodes.push(affected)
-		graph.value.links.push(link)
-	}
-	if (g == "löschen"){
-		graph.value.nodes = graph.value.nodes.filter((node) => node.id != selection.value)
-		graph.value.links = graph.value.links.filter((link) => link.target.id != selection.value)
-		graph.value.links = graph.value.links.filter((link) => link.source.id != selection.value)
-	}
-	graph.value.hasUpdate = true
+function onBackground() {
+	selection.value = [];
+	controlItems.value.forEach((item) => {
+		item.enabled = item.checkEnabled();
+	});
 }
-function onClick(e,d){
-	selection.value = [d.id]
-}
-function onBackground(){
-	selection.value = []
-}
+
+onMounted(() => {
+	loadOperations(() => {
+		availableRessources();
+	});
+	setInterval(() => {
+		loadOperations(() => {
+			availableRessources();
+		});
+	}, 5000);
+	loadOperation(operationID, rawGraph, () => {
+		availableRessources();
+		createGraph();
+		controlItems.value.forEach((item) => {
+			item.enabled = item.checkEnabled();
+		});
+	});
+});
 </script>
 
 <style lang="scss" scoped>
